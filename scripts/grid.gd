a7197746b29facc84d2261d2d7911c935be91eef
marianZamorano <mzamorano295@gmail.com>
2025-09-07 17:35:07 -0400

    piecitas especiaales

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -4,6 +4,8 @@ extends Node2D
 enum {WAIT, MOVE}
 var state
 
+enum {HORIZONTAL, VERTICAL}
+
 # grid
 @export var width: int
 @export var height: int
@@ -21,6 +23,32 @@ var possible_pieces = [
 	preload("res://scenes/yellow_piece.tscn"),
 	preload("res://scenes/orange_piece.tscn"),
 ]
+var column_pieces = {
+	"blue": preload("res://scenes/column_blue_piece.tscn"),
+	"green": preload("res://scenes/column_green_piece.tscn"),
+	"light_green": preload("res://scenes/column_light_green_piece.tscn"),
+	"pink": preload("res://scenes/column_pink_piece.tscn"),
+	"yellow": preload("res://scenes/column_yellow_piece.tscn"),
+	"orange": preload("res://scenes/column_orange_piece.tscn")
+}
+
+var row_pieces = {
+	"blue": preload("res://scenes/row_blue_piece.tscn"),
+	"green": preload("res://scenes/row_green_piece.tscn"),
+	"light_green": preload("res://scenes/row_light_green_piece.tscn"),
+	"pink": preload("res://scenes/row_pink_piece.tscn"),
+	"yellow": preload("res://scenes/row_yellow_piece.tscn"),
+	"orange": preload("res://scenes/row_orange_piece.tscn")
+}
+
+var color_piece = {
+	"blue": preload("res://scenes/color_blue_piece.tscn"),
+	"green": preload("res://scenes/color_green_piece.tscn"),
+	"light_green": preload("res://scenes/color_light_green_piece.tscn"),
+	"pink": preload("res://scenes/color_pink_piece.tscn"),
+	"yellow": preload("res://scenes/color_yellow_piece.tscn"),
+	"orange": preload("res://scenes/color_orange_piece.tscn")	
+}
 # current pieces in scene
 var all_pieces = []
 
@@ -38,55 +66,26 @@ var is_controlling = false
 
 # scoring variables and signals
 var score = 0
-var final_score = 0
 signal score_changed(new_score)
 
 # counter variables and signals
-var moves_left = 10
-signal moves_changed(new_moves)
+var minus_moves = 1
+signal moves_left_changed(new_moves)
 
-# levels variables and signal to change levels
-var current_level = 1
-var target_score = 10 
-var level_time = 10  
-var time_remaining = level_time
-var time_passed = 0
-signal level_changed(new_level)
-signal time_remaining_changed(new_time)
-signal game_over_signal()
-signal current_level_signal()
-	
-#Called when the node enters the scene tree for the first time.
+var just_moved = []
+var groups = []
+
+
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Nodo padre: ", get_parent().name)
 	state = MOVE
 	randomize()
 	all_pieces = make_2d_array()
+	just_moved = make_2d_array()
 	spawn_pieces()
-	emit_signal("moves_changed", moves_left)
-	start_level_timer()
-	call_deferred("emit_signal", "current_level_signal", current_level)
-	
-	
-func _process(delta):
-	if state == MOVE:
-		touch_input()
-	
-	# Update the timer
-	time_passed += delta
-	if time_passed >= 1:
-		time_remaining -= 1
-		emit_signal("time_remaining_changed", time_remaining)
-		time_passed = 0
 
-		if time_remaining <= 0:
-			check_end_of_level()
-			
-func start_level_timer():
-	var timer_label = get_parent().get_node("top_ui/MarginContainer/HBoxContainer/timer_label")
-	if timer_label:
-		timer_label.text = str(time_remaining) 
-	
 func make_2d_array():
 	var array = []
 	for i in width:
@@ -168,12 +167,12 @@ func swap_pieces(column, row, direction: Vector2):
 	other_piece.move(grid_to_pixel(column, row))
 	
 	if not move_checked:
+		just_moved[column][row] = true
+		just_moved[column + direction.x][row + direction.y] = true
 		var isFind = find_matches()
 		if isFind:
-			moves_left -= 1
-			emit_signal("moves_changed", moves_left)
-			check_end_of_level()
-			
+			# Restar una jugada
+			emit_signal("moves_left_changed", minus_moves)
 
 func store_info(first_piece, other_piece, place, direction):
 	piece_one = first_piece
@@ -187,8 +186,15 @@ func swap_back():
 	state = MOVE
 	move_checked = false
 
+func init_just_moved():
+	for i in width:
+		for j in height:
+			just_moved[i][j] = false
+
 func touch_difference(grid_1, grid_2):
 	var difference = grid_2 - grid_1
+	
+	init_just_moved()
 	# should move x or y?
 	if abs(difference.x) > abs(difference.y):
 		if difference.x > 0:
@@ -201,11 +207,13 @@ func touch_difference(grid_1, grid_2):
 		elif difference.y < 0:
 			swap_pieces(grid_1.x, grid_1.y, Vector2(0, -1))
 
-#func _process(delta):
-	#if state == MOVE:
-		#touch_input()
+func _process(delta):
+	if state == MOVE:
+		touch_input()
 
 func find_matches():
+	groups = []
+	var idGroup = make_2d_array()
 	var matches_found = false
 	for i in width:
 		for j in height:
@@ -219,6 +227,30 @@ func find_matches():
 					and 
 					all_pieces[i - 1][j].color == current_color and all_pieces[i + 1][j].color == current_color
 				):
+					var group = null
+					if idGroup[i-1][j] != null:
+						group=idGroup[i-1][j]
+					if idGroup[i][j] != null:
+						group=idGroup[i][j]
+					if idGroup[i+1][j] != null:
+						group=idGroup[i+1][j]	
+					
+					if(group == null):
+						idGroup[i-1][j]=groups.size()
+						idGroup[i][j]=groups.size()
+						idGroup[i+1][j]=groups.size()
+						group = groups.size()
+						groups.append([])
+							
+					groups[group].append([Vector2(i,j), all_pieces[i][j].color, VERTICAL])
+					groups[group].append([Vector2(i+1,j), all_pieces[i+1][j].color, VERTICAL])
+					groups[group].append([Vector2(i-1,j), all_pieces[i-1][j].color, VERTICAL])
+					
+					all_pieces[i][j].special(Vector2(i,j), all_pieces)	
+					all_pieces[i-1][j].special(Vector2(i-1,j), all_pieces)
+					all_pieces[i+1][j].special(Vector2(i+1,j), all_pieces)
+															
+					
 					all_pieces[i - 1][j].matched = true
 					all_pieces[i - 1][j].dim()
 					all_pieces[i][j].matched = true
@@ -226,7 +258,6 @@ func find_matches():
 					all_pieces[i + 1][j].matched = true
 					all_pieces[i + 1][j].dim()
 					score += 30 
-					final_score += 30
 					emit_signal("score_changed", score)
 					matches_found = true
 				# detect vertical matches
@@ -237,6 +268,30 @@ func find_matches():
 					and 
 					all_pieces[i][j - 1].color == current_color and all_pieces[i][j + 1].color == current_color
 				):
+					
+					var group = null
+					if idGroup[i][j-1] != null:
+						group=idGroup[i][j-1]
+					if idGroup[i][j] != null:
+						group=idGroup[i][j]
+					if idGroup[i][j+1] != null:
+						group=idGroup[i][j+1]	
+					
+					if(group == null):
+						idGroup[i][j-1]=groups.size()
+						idGroup[i][j]=groups.size()
+						idGroup[i][j+1]=groups.size()
+						group = groups.size()
+						groups.append([])
+							
+					groups[group].append([Vector2(i,j-1), all_pieces[i][j-1].color, HORIZONTAL])
+					groups[group].append([Vector2(i,j), all_pieces[i][j].color, HORIZONTAL])
+					groups[group].append([Vector2(i,j+1), all_pieces[i][j+1].color, HORIZONTAL])
+					
+					all_pieces[i][j].special(Vector2(i,j), all_pieces)
+					all_pieces[i][j+1].special(Vector2(i,j+1), all_pieces)
+					all_pieces[i][j-1].special(Vector2(i,j-1), all_pieces)
+					
 					all_pieces[i][j - 1].matched = true
 					all_pieces[i][j - 1].dim()
 					all_pieces[i][j].matched = true
@@ -244,15 +299,24 @@ func find_matches():
 					all_pieces[i][j + 1].matched = true
 					all_pieces[i][j + 1].dim()
 					score += 30 
-					final_score += 30
 					emit_signal("score_changed", score)
 					matches_found = true
 					
 	get_parent().get_node("destroy_timer").start()
+
 	return matches_found
 	
+		
+func uniqueArray(array):
+	var unique = []
+	for item in array:
+		if not unique.has(item):
+			unique.append(item)
+	return unique
+
 func destroy_matched():
 	var was_matched = false
+	
 	for i in width:
 		for j in height:
 			if all_pieces[i][j] != null and all_pieces[i][j].matched:
@@ -263,21 +327,54 @@ func destroy_matched():
 	move_checked = true
 	if was_matched:
 		get_parent().get_node("collapse_timer").start()
-		
 	else:
 		swap_back()
 
+func create_special(size, position, color, direction):
+	var piece = null
+	if(size == 4):
+		if direction == VERTICAL:
+			piece = row_pieces[color].instantiate()
+		else:
+			piece = column_pieces[color].instantiate()
+	else:
+		piece = color_piece[color].instantiate()
+	
+	add_child(piece)
+	piece.position = grid_to_pixel(position[0], position[1] - y_offset)
+	piece.move(grid_to_pixel(position[0], position[1]))
+	
+	all_pieces[position[0]][position[1]] = piece
+	
+func generate_special():
+	for group in groups:
+		group = uniqueArray(group)
+		if group.size() >= 4:
+			var special
+			for piece in group:
+				var x = piece[0][0]
+				var y = piece[0][1]
+				if just_moved[x][y]:
+					special = piece
+					break
+			create_special(group.size(), special[0], special[1], special[2])
+	groups = []
+
 func collapse_columns():
+	
+	generate_special()
+	
 	for i in width:
 		for j in height:
 			if all_pieces[i][j] == null:
-				print(i, j)
+				print(i, ", ", j)
 				# look above
 				for k in range(j + 1, height):
 					if all_pieces[i][k] != null:
 						all_pieces[i][k].move(grid_to_pixel(i, j))
 						all_pieces[i][j] = all_pieces[i][k]
 						all_pieces[i][k] = null
+						just_moved[i][j] = true
 						break
 	get_parent().get_node("refill_timer").start()
 
@@ -327,53 +424,6 @@ func _on_collapse_timer_timeout():
 func _on_refill_timer_timeout():
 	refill_columns()
 	
-#func _on_level_timer_timeout():
-	#time_remaining -= 1
-	#emit_signal("time_remaining_changed", time_remaining)
-	#if time_remaining <= 0:
-		#check_end_of_level()
-		
-func check_end_of_level():
-	if score >= target_score and (moves_left <= 0 or time_remaining <= 0):
-		advance_to_next_level()
-	elif moves_left <= 0 or time_remaining <= 0:
-		game_over()
-		
-func advance_to_next_level():
-	current_level += 1
-	target_score += 100
-	level_time += 10  
-	emit_signal("level_changed", current_level) 
-	reset_level()
-	
-func reset_level():
-	score = 0
-	time_remaining = level_time
-	reset_moves()
-	emit_signal("score_changed", score)
-	emit_signal("time_remaining_changed", time_remaining)
-	emit_signal("current_level_signal", current_level)
-	reset_moves()
-	reset_board()
-	start_level_timer()
-	
-func reset_board():
-	for i in width:
-		for j in height:
-			if all_pieces[i][j] != null:
-				all_pieces[i][j].queue_free()
-				all_pieces[i][j] = null
-	# Generate new pieces
-	spawn_pieces()
-	
-func reset_moves():
-	moves_left = 10 + 2 * (current_level - 1)
-	emit_signal("moves_changed", moves_left)
-	
 func game_over():
 	state = WAIT
 	print("game over")
-	get_tree().paused = true
-	
-	#Final score and current levell
-	emit_signal("game_over_signal", final_score, current_level)
